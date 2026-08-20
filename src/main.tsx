aaad175923b288ca2de5fc36d0533e3447f50f5a
Blomska <blomenkemper.kai@gmail.com>
2026-08-20 14:18:55 +0200

    Add Vite, TypeScript, Tailwind and GitHub Pages configuration

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './amsterdam_reiseportal'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
